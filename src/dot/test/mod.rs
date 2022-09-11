ec5c8bd6eae1b74b9065e049c8a45b9cfb92a51a
Paul Volavsek <paul.volavsek@gmail.com>
2022-09-11 15:17:25 +0200

    :white_check_mark:

--- src/dot/test/mod.rs
+++ src/dot/test/mod.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use speculoos::prelude::*;
 
 use super::read_dots;
+use crate::helpers::Select;
 
 mod data;
 
@@ -11,10 +12,28 @@ fn read_all_dots() {
   let dots = read_dots(Path::new(file!()).parent().unwrap().join("data/directory_structure").as_path(), &["**".to_owned()], &Default::default()).unwrap();
 
   assert_that!(dots).has_length(4);
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test01");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test02");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test03/test04");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test03/test05");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test01");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test02");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test03/test04");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test03/test05");
+
+  assert_that!(dots.iter().find(|d| d.0 == "/test02"))
+    .is_some()
+    .select(|d| &d.1.depends)
+    .is_some()
+    .contains(&"/test01".to_owned());
+
+  assert_that!(dots.iter().find(|d| d.0 == "/test03/test04"))
+    .is_some()
+    .select(|d| &d.1.depends)
+    .is_some()
+    .contains(&"/test02".to_owned());
+
+  assert_that!(dots.iter().find(|d| d.0 == "/test03/test05"))
+    .is_some()
+    .select(|d| &d.1.depends)
+    .is_some()
+    .contains(&"/test03/test04".to_owned());
 }
 
 #[test]
@@ -27,8 +46,8 @@ fn read_sub_dots() {
   .unwrap();
 
   assert_that!(dots).has_length(2);
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test03/test04");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test03/test05");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test03/test04");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test03/test05");
 }
 
 #[test]
@@ -36,8 +55,8 @@ fn read_non_sub_dots() {
   let dots = read_dots(Path::new(file!()).parent().unwrap().join("data/directory_structure").as_path(), &["*".to_owned()], &Default::default()).unwrap();
 
   assert_that!(dots).has_length(2);
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test01");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test02");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test01");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test02");
 }
 
 #[test]
@@ -45,7 +64,7 @@ fn read_all_file_formats() {
   let dots = read_dots(Path::new(file!()).parent().unwrap().join("data/file_formats").as_path(), &["**".to_owned()], &Default::default()).unwrap();
 
   assert_that!(dots).has_length(3);
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test01");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test02");
-  assert_that!(dots).mapped_contains(|d| &d.0, &"test03");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test01");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test02");
+  assert_that!(dots).mapped_contains(|d| &d.0, &"/test03");
 }
